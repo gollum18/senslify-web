48fd067ce5d15f250fd976910d67fcfeba7bd5f6
Christen Ford <christen0ford@gmail.com>
2019-07-20 12:42:49 -0400

    update sensors router

--- senslify/sensors.py
+++ senslify/sensors.py
@@ -2,26 +2,35 @@ import aiohttp_jinja2
 
 
 @aiohttp_jinja2.template('sensors/info.jinja2')
-def info_handler(request):
+async def info_handler(request):
     '''
-    Defines a GET endpoint for the sensor info page.
+    Defines a POST endpoint for the sensor info page.
     Arguments:
         request: A aiohttp.web.Request object.
     '''
-    pass
+    # get the stuff needed to build the response
+    host = request.app['config'].host
+    protocol = 'wss://' if request.secure else 'ws://'
+    sensor = request.query['sensor']
+    # build the response thru jinja2
+    return {'title': 'Sensor Info',
+            'sensor': sensor,
+            'ws_url': protocol + host + '/ws'}
 
 
 @aiohttp_jinja2.template('sensors/sensors.jinja2')
-def sensors_handler(request):
+async def sensors_handler(request):
     '''
     Defines a GET endpoint for the sensors listing page.
     Arguments:
         request: A aiohttp.web.Request object.
     '''
-    pass
+    # TODO: The information on this page needs caching,
+    #   it will take a long time to load otherwise
+    return {'title': 'Sensors'}
 
 
-def upload_handler(request):
+async def upload_handler(request):
     '''
     Defines a POST endpoint for uploading sensor data.
     Arguments:
