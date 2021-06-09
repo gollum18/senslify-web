59a9360b632ab0924249796688061ca10df6a9e8
Christen Ford <christen0ford@gmail.com>
2021-06-09 01:47:49 -0400

    refactor client/server to use groupid as well for room identification

--- senslify/sockets.py
+++ senslify/sockets.py
@@ -25,88 +25,94 @@ from senslify.filters import filter_reading
 # Define WebSocket command methods
 #
 
-def _does_room_exist(rooms, sensorid):
+def _does_room_exist(rooms, groupid, sensorid):
     """ Determines if there is a room for a given sensor or not.
 
     Args:
         rooms (dict): A dictionary containing sensor rooms.
-        sensorid (int): The sensorid corresponding to the room to check for.
+        groupid (int): The groupid corresponding to the room to check.
+        sensorid (int): The sensorid corresponding to the room to check.
     """
-    if not sensorid in rooms:
+    if not (groupid, sensorid) in rooms:
         return False
     return True
 
 
-def _does_ws_exist(rooms, sensorid, ws):
+def _does_ws_exist(rooms, groupid, sensorid, ws):
     """Determines if a WebSocket exists in the given room or not.
 
     Args:
         rooms (dict): A dictionary containing sensor rooms.
+        groupid (int): The groupid corresponding to the room to check attendance for.
         sensorid (int): The sensorid corresponding to the room to check attendance for.
         ws (aiohttp.web.WebSocketResponse): The WebSocket to check for.
     """
-    if not _does_room_exist(rooms, sensorid):
+    if not _does_room_exist(rooms, groupid, sensorid):
         return False
-    if ws not in rooms[sensorid]:
+    if ws not in rooms[(groupid, sensorid)]:
         return False
     return True
 
 
-async def _leave(rooms, sensorid, ws):
+async def _leave(rooms, groupid, sensorid, ws):
     """Allows a WebSocket to leave a room
 
     Args:
         rooms (dict): A dictionary contaiing sensor rooms.
+        groupid (int): The groupid corresponding to the room to leave.
         sensorid (int): The sensorid corresponding to the room to leave.
         ws (aiohttp.web.WebSocketResponse): The WebSocket requesting to leave the room.
     """
     # only delete the ws from the room if room exists and the ws is in the room
-    if not _does_ws_exist(rooms, sensorid, ws):
+    if not _does_ws_exist(rooms, groupid, sensorid, ws):
         return
-    del rooms[sensorid][ws]
+    del rooms[(groupid, sensorid)][ws]
 
 
-async def _join(rooms, sensorid, ws):
+async def _join(rooms, groupid, sensorid, ws):
     """Allows a WebSocket to join a room.
 
     Args:
         rooms (dict): A dictionary containing sensor rooms.
+        groupid (int): The groupid corresponding to the room to join.
         sensorid (int): The sensorid corresponding to the room to join.
         ws (aiohttp.web.WebSocketResponse): The WebSocket to add to the room.
     """
     # create the room if it does not exist
     if sensorid not in rooms:
-        rooms[sensorid] = dict()
+        rooms[(groupid, sensorid)] = dict()
     # add the client to the room if its not already there, default to temp
-    if ws not in rooms[sensorid]:
-        rooms[sensorid][ws] = 0
+    if ws not in rooms[(groupid, sensorid)]:
+        rooms[(groupid, sensorid)][ws] = 0
 
 
-async def _change_stream(rooms, sensorid, ws, rtype):
+async def _change_stream(rooms, groupid, sensorid, ws, rtype):
     """Changes the data stream the WebSocket receives.
 
     Args:
         rooms (dict): A dictionary containing sensor rooms.
+        groupid (int): The groupid corresponding to the room the WebSocket is in.
         sensorid (int): The sensorid corresponding to the room the WebSocket is in.
         ws (aiohttp.web.WebSocketResponse): The WebSocket to change stream for.
         rtype (int): The stream type to change to.
     """
     # check if the ws exists, return if so
-    if not _does_ws_exist(rooms, sensorid, ws):
+    if not _does_ws_exist(rooms, groupid, sensorid, ws):
         return
-    rooms[sensorid][ws] = int(rtype)
+    rooms[(groupid, sensorid)][ws] = int(rtype)
 
 
-async def message(rooms, sensorid, msg):
+async def message(rooms, groupid, sensorid, msg):
     """Sends a message to the participants of a room.
 
     Args:
         rooms (dict): A dictionary containing the sensor rooms.
+        groupid: The groupid corresponding to the room to message.
         sensorid (int): The sensorid corresponding to the room to message.
         msg (dict): The message to send to all room participants (usually a reading).
     """
     # only send the message if the room exists
-    if not _does_room_exist(rooms, sensorid):
+    if not _does_room_exist(rooms, groupid, sensorid):
         return
     # add additional fields to the message
     # create the response object for the websocket
@@ -125,7 +131,7 @@ async def message(rooms, sensorid, msg):
         print("ERROR: KeyError has occurred sending message, 'rtypeid' not found!")
         return
     # steps through all clients in the room
-    for ws, rtype in rooms[sensorid].items():
+    for ws, rtype in rooms[(group, sensorid)].items():
         if rtype == rtypeid:
             await ws.send_str(simplejson.dumps(resp))
 
@@ -159,12 +165,13 @@ async def ws_handler(request):
             if not js:
                 continue
             
-            # make sure the cmd and sensorid fields are present, they are
+            # make sure the cmd, groupid and sensorid fields are present, they are
             #   required for command execution
-            if 'cmd' not in js or 'sensorid' not in js:
+            if 'cmd' not in js or 'groupid' not in js or 'sensorid' not in js:
                 continue
             cmd = js['cmd'];
             try:
+                groupid = int(js['groupid'])
                 sensorid = int(js['sensorid'])
             except Exception:
                 continue
@@ -172,25 +179,24 @@ async def ws_handler(request):
             # adds the requesting websocket as a receiver for messages from
             #   the indicated sensor
             if cmd == 'RQST_JOIN':
-                await _join(request.app['rooms'], sensorid, ws)
+                await _join(request.app['rooms'], groupid, sensorid, ws)
             # close the connection if the client requested it
             elif cmd == 'RQST_CLOSE':
-                await _leave(request.app['rooms'], sensorid, ws)
+                await _leave(request.app['rooms'], groupid, sensorid, ws)
                 await ws.close()
                 break
             # handle requests from users to switch to a different reading type
             elif cmd == 'RQST_STREAM':
                 # perform verification checks
-                if 'groupid' not in js or 'rtypeid' not in js:
+                if 'rtypeid' not in js:
                     continue
                 # get request info
                 try:
-                    groupid = int(js['groupid'])
                     rtypeid = int(js['rtypeid'])
                 except Exception:
                     continue
                 # change the stream
-                await _change_stream(request.app['rooms'], sensorid, ws, rtypeid)
+                await _change_stream(request.app['rooms'], groupid, sensorid, ws, rtypeid)
                 # construct a response containing the top 100 readings for the stream
                 resp = dict()
                 resp['cmd'] = 'RESP_STREAM'
@@ -215,7 +221,6 @@ async def ws_handler(request):
                     continue
                 # get request info
                 try:
-                    groupid = int(js['groupid'])
                     rtypeid = int(js['rtypeid'])
                     start_date = int(js['start_date'])
                     end_date = int(js['end_date'])
@@ -235,7 +240,7 @@ async def ws_handler(request):
         elif msg.type == aiohttp.WSMsgType.ERROR:
             ws.send_str('WebSocket encountered an error: %s\nPlease refresh the page.'.format(ws.exception()))
 
-    await _leave(request.app['rooms'], sensorid, ws)
+    await _leave(request.app['rooms'], groupid, sensorid, ws)
 
     return ws
 
@@ -248,8 +253,8 @@ async def socket_shutdown_handler(app):
         app (aiohttp.web.Application): The web application hosting the sensor rooms.
     """
     # close any open websockets
-    for sensor in app['rooms'].keys():
-        for ws in app['rooms'][sensor].keys():
+    for groupid, sensor in app['rooms'].keys():
+        for ws in app['rooms'][(groupid, sensor)].keys():
             if not ws.closed:
                 # close the WebSocket
                 await ws.close(code=aiohttp.WSCloseCode.GOING_AWAY,
